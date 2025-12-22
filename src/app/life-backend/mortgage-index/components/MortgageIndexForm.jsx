f3ded7fc8677da189a6ce29537f36b56f3a7986e
404manveer <manveershah007@gmail.com>
2025-12-22 17:36:40 +0530

    fix

--- src/app/life-backend/mortgage-index/components/MortgageIndexForm.jsx
+++ src/app/life-backend/mortgage-index/components/MortgageIndexForm.jsx
@@ -78,7 +78,7 @@ export default function MortgageIndexForm({initialData}){
            <ImageInput
            uploadAction={uploadPageFile}
            onChange={handleImageSet} 
-           initialImage={initialData.image || ''}
+           initialImage={initialData?.image || ''}
             />
              <MetaTags
                metaTitle={formData?.metaTitle}
