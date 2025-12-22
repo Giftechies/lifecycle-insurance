7e0e8138c5384e4745346a71d8012aeef5abfa3d
404manveer <manveershah007@gmail.com>
2025-12-22 17:06:12 +0530

    fix

--- src/app/life-backend/business-funding/page.jsx
+++ src/app/life-backend/business-funding/page.jsx
@@ -3,16 +3,16 @@ import BusinessFundingTable from "./components/BusinessFundingTable";
 import { getBusinessFunding } from "@/actions/businessFunding.actions";
 
 export default async function BusinessPage() {
-  const { data } = await getBusinessFunding();
+  // const { data } = await getBusinessFunding();
   return (
     <>
-      <div className="dashboard-header">
+      {/* <div className="dashboard-header">
         <h3>Business Funding</h3>
         <Link href="/life-backend/business-funding/add">
           Add Business Funding
         </Link>
       </div>
-      <BusinessFundingTable initialData={data} />
+      <BusinessFundingTable initialData={data} /> */}
     </>
   );
 }
